e298ab881a581fe16d9eb79c829a1ffeb140f4bb
Ark4N <112132732+Arkk4n@users.noreply.github.com>
2026-07-01 21:19:11 +0200

    Implement winner checking

--- higher_lower/higher_lower.py
+++ higher_lower/higher_lower.py
@@ -7,13 +7,31 @@ def get_random_account():
     return random.choice(game_data.data)
 
 
-account_a = get_random_account()
-account_b = get_random_account()
+def get_winner(account_a, account_b):
+    if account_a["follower_count"] > account_b["follower_count"]:
+        return "a"
+    else:
+        return "b"
 
-print(
-    f"Compare A: {account_a['name']}, {account_a['description']}, {account_a['country']}"
-)
-print(art.vs)
-print(
-    f"Compare B: {account_b['name']}, {account_b['description']}, {account_b['country']}"
-)
+
+def main():
+    account_a = get_random_account()
+    account_b = get_random_account()
+    print(
+        f"Compare A: {account_a['name']}, {account_a['description']}, {account_a['country']}"
+    )
+    print(art.vs)
+    print(
+        f"Compare B: {account_b['name']}, {account_b['description']},{account_b['country']}"
+    )
+    guess = input("Who has more followers? Type A or B: ").lower().strip()
+    if guess not in ["a", "b"]:
+        print("Please enter A or B")
+    winner = get_winner(account_a, account_b)
+    if guess == winner:
+        print("You win")
+    else:
+        print("You lose")
+
+
+main()
