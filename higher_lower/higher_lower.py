6c0a59aa9fefba1ee4d5b2d075f2ebc24b352cce
Ark4N <112132732+Arkk4n@users.noreply.github.com>
2026-07-02 18:18:02 +0200

    Complete higher lower game

--- higher_lower/higher_lower.py
+++ higher_lower/higher_lower.py
@@ -15,23 +15,44 @@ def get_winner(account_a, account_b):
 
 
 def main():
+    score = 0
     account_a = get_random_account()
     account_b = get_random_account()
-    print(
-        f"Compare A: {account_a['name']}, {account_a['description']}, {account_a['country']}"
-    )
-    print(art.vs)
-    print(
-        f"Compare B: {account_b['name']}, {account_b['description']}, {account_b['country']}"
-    )
-    guess = input("Who has more followers? Type A or B: ").lower().strip()
-    if guess not in ["a", "b"]:
-        return "Invalid input"
-    winner = get_winner(account_a, account_b)
-    if guess == winner:
-        print("You win")
-    else:
-        print("You lose")
+    while True:
+        print(
+            f"Compare A: {account_a['name']}, {account_a['description']}, {account_a['country']}"
+        )
+        print(art.vs)
+        print(
+            f"Compare B: {account_b['name']}, {account_b['description']}, {account_b['country']}"
+        )
+        guess = input("Who has more followers? Type A or B: ").lower().strip()
+        if guess not in ["a", "b"]:
+            print("Invalid input")
+            continue
+        winner = get_winner(account_a, account_b)
+        if guess == winner:
+            print("You win")
+            account_a = account_b
+            account_b = get_random_account()
+            while account_a == account_b:
+                account_b = get_random_account()
+            score += 1
+            print(f"Current score {score}")
+        else:
+            print("You lose")
+            break
+    print(f"Your score is {score}")
 
 
 main()
+
+
+# TODO:
+# - Add while loop - OK
+# - Keep score - OK
+# - Move account_b to account_a - OK
+# - Generate new account_b - OK
+
+# FIXME:
+# Duplicate accounts can appear - DONE
